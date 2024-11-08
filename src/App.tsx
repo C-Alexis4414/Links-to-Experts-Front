9aac80ed93b6e7c6d3b2ff1a1f23047b399bb534
C-Alexis4414 <a.chentre@protonmail.com>
2024-11-08 12:52:14 +0100

    YL-65 WIP:Implementing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import HomePage from './homePage/page';
+import HomePage from './pages/homePage';
 import RootLayout from './layout';
 import Navbar from './components/navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
+import Register from './pages/register';
+import PageNotFound from './pages/pageNotFound';
 
 function App() {
+  // return (
+  //   <RootLayout>
+  //     <Navbar />
+  //     <HomePage />
+  //   </RootLayout>
+  // );
   return (
-    <RootLayout>
-      <Navbar />
-      <HomePage />
-    </RootLayout>
+  <RootLayout>
+    <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </RootLayout>
   );
 }
 
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserCard from '../components/userCard/userCard'
+
+function HomePage() {
+    return (<>
+        <UserCard name="Gianni" role="Dev" avatarSrc="Links-to-Experts-Front\src\app\pages\page.tsx" />
+    </>)
+}
+
+export default HomePage
