584d71631b3ece8660995e30db8d3fd388c85db6
C-Alexis4414 <a.chentre@protonmail.com>
2024-12-16 21:04:08 +0100

    YL-68 Edit button, avatar and userName

--- src/components/userInfoCard/userInfoCard.tsx
+++ src/components/userInfoCard/userInfoCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Card, CardContent, Typography, Box, IconButton } from '@mui/material'
+import { Card, CardContent, Typography, Box, IconButton, Tooltip } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -9,7 +9,6 @@ import { set } from 'react-hook-form';
 const UserInfoCard: React.FC = () => {
     const { user } = useAuth();
     const navigate = useNavigate();
-    const [tagChannel, setTagChannel] = useState<string | null>(null);
 
     const handleEditClick = () => {
         navigate('/profile/edit');
@@ -30,20 +29,26 @@ if (!user) {
 }
     return (
         <Card sx={{ maxWidth: 600, margin: 'auto', mt: 4, padding: 2 }}>
-            <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItem: 'center' }}>
+            <CardContent sx={{ display: 'flex', justifyContent: 'space-between', alignItem: 'center', flexDirection: 'row' }}>
                 <Box>
                     <Typography variant="h5" component="div" gutterBottom>
                         Mes infos
                     </Typography>
                     <Typography variant="body1" color="text.secondary">
-                        {user.userName}
+                        {user.is_Youtuber && 'Youtuber'}
+                        {user.is_Youtuber && user.is_Professional && ' / '}
+                        {user.is_Professional && 'Professionnel'},
+                        {user.tagChannel && ` ${user.tagChannel}`}
+                        {user.tagChannel && user.urlLinkedin && ' / '}
+                        {user.urlLinkedin && ` ${user.urlLinkedin}`},
                     </Typography>
-                    <Typography variant="body1" color="text.secondary">
-                        {user.tagChannel}
-                    </Typography>
-                    <Typography variant="body1" color="text.secondary">
-                        {user.email}
-                    </Typography>
+                </Box>
+                <Box display="flex" alignItems="center">
+                    <Tooltip title="Modifier les infos utilisateur" placement="top">
+                        <IconButton onClick={handleEditClick} color="primary">
+                            <EditIcon />
+                        </IconButton>
+                    </Tooltip>
                 </Box>
             </CardContent>
         </Card>
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Box } from '@mui/material'
+import { Button, Box, Avatar, Typography } from '@mui/material'
 import { useAuth } from '../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import Grid from '@mui/material/Grid2';
@@ -7,7 +7,7 @@ import UserInfoCard from '../components/userInfoCard/userInfoCard';
 
 const Profile = () => {
     const [error, setError] = useState<string | null>(null);
-    const { deleteUser } = useAuth();
+    const { user, deleteUser } = useAuth();
     const navigate = useNavigate();
 
     const handleDelete = async (e: React.FormEvent) => {
@@ -21,6 +21,11 @@ const Profile = () => {
         }
     };
 
+    const userInitial = user?.userName?.charAt(0).toUpperCase() || '';
+
+if (!user) {
+    return null;
+}
     return (
         <Box
             sx={{
@@ -31,10 +36,18 @@ const Profile = () => {
             }}
         >
             <Grid container spacing={2}>
+                <Grid size={12} display="flex" justifyContent="center" alignItems="center" flexDirection="column">
+                        <Avatar alt={user?.userName} src="" sx={{ width: 120, height: 120, fontSize: 96, margin: 1 }}>
+                            {userInitial}
+                        </Avatar>
+                        <Typography variant="h5" component="div">
+                            {user?.userName}
+                        </Typography>
+                </Grid>
                 <Grid size={12}>
                     <UserInfoCard />
                 </Grid>
-                <Grid size={12}>
+                <Grid size={12} display="flex" justifyContent="center" alignItems="center">
                     <Button 
                         type='submit' 
                         variant='contained' 
